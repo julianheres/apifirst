0bc774a123d22f099d05bc36fa9cb217bbf0b5ca
ZzzRrr <wirelesser@hotmail.com>
2024-03-07 00:12:54 +0800

    initial commit

---
+++ apifirst-maven-plugin/src/main/java/com/zhourui/ApiSpec.java
@@ -0,0 +1,9 @@
+package com.zhourui;
+
+import java.util.Map;
+
+public class ApiSpec {
+    public String url;
+    public String type;
+    public Map<?,?> configOptions;
+}
---
+++ apifirst-maven-plugin/src/main/java/com/zhourui/OpenApiConfig.java
@@ -0,0 +1,8 @@
+package com.zhourui;
+
+import java.util.List;
+
+public class OpenApiConfig {
+    public String lib;
+    public List<ApiSpec> apiSpecs;
+}
